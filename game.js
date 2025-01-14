cb92776ecda4038e4e91dc42a9304fe39642e7c8
Aoife Muckian <dhv3af1l@students.codeinstitute.net>
2025-01-14 17:40:37 +0000

    'detailed to specific buttons'

---
+++ game.js
@@ -0,0 +1,46 @@
+const textElement = document.getElementById('text')
+const optionButtons = document.getElementById('options')
+let state = {}
+function startGame() {
+    state = {}
+}
+function showTextNode(textNodeIndex) {
+    const textNode = textNodes.find(textNode => text.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    while (optionButtonsElement.firstChild) {
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+    }
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+
+        }
+    })
+}
+function showOption(option) {
+    const button = document.createElement('button')
+    button.innerText = option.text
+    button.classList.add('btn')
+    button.addEventListener('click', () => selectOption())
+    optionButtonsElement.appendChild(button)
+}
+function selectOption(option) {
+
+}
+const textNodes = [
+    {
+        id: 1,
+        text: "You have recently moved to a small village in rural Wales. You receive an invite for a party.",
+        options: [
+            {
+                text: 'RVSP',
+                setState: { party: true },
+                nextText: 2
+            },
+            {
+                text: 'Ignore',
+                nextText: 2
+            }
+        ]
+    }
+]
+startGame()
